27f719ba2e223ec4d784fcc86477c69a99d9b369
Eliot Hertenstein <39346975+EliotHertenstein@users.noreply.github.com>
2021-06-16 12:52:23 -0700

    Add customs stuff
    Thank

--- _next/static/1vcw7Gml4OLVQGKfAUFAO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2f886c0f4cb1059f7e78.css","static/chunks/pages/index-bbc7e7dc195272258fc0.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/nfHLkcXVkaCRMMqGSW606/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/2f886c0f4cb1059f7e78.css","static/chunks/pages/index-a65369f1f575a735d17b.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
